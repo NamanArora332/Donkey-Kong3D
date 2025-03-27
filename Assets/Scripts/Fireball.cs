a6185a923a4dccba57416d76070da14cd6d5782a
dabby04 <97193496+dabby04@users.noreply.github.com>
2025-03-27 09:14:06 -0700

    Added back and forth logic

--- Assets/Scripts/Fireball.cs
+++ Assets/Scripts/Fireball.cs
@@ -11,9 +11,9 @@ public class Fireball : MonoBehaviour
     public AudioClip explosionSound;
     public GameObject Player;
     public Ladder[] ladders;
-    public int currentLadder=0;
+    public int currentLadder = 0;
 
-void Start()
+    void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
@@ -21,31 +21,29 @@ void Start()
     {
         // transform.Translate(Vector3.right * speed * Time.deltaTime); //commented out because it gives fireball jumpy motion
         lifetime -= Time.deltaTime;
-        if(lifetime >0) {
-            MoveTowardsPlayer();
-            }
-        else
+        if (lifetime <= 0)
         {
             Destroy(gameObject);
         }
-        
+        else
+        {
+            MoveTowardsPlayer();
+        }
+
 
     }
     void MoveTowardsPlayer()
     {
         //check if player is on same level as fireball
-        
+
         if (Player.transform.position.y <= ladders[currentLadder].top.position.y)
         {
             Debug.Log("Player is on same level as fireball");
             // Determine movement direction (-1 for left, 1 for right)
-            float moveDirection = Mathf.Sign(ladders[currentLadder].top.position.x - transform.position.x);
+            float moveDirection = Mathf.Sign(Player.transform.position.x - transform.position.x);
 
             // Set velocity to move only in the X direction
-            rb.linearVelocity = new Vector3(moveDirection * speed, rb.linearVelocity.y, rb.linearVelocity.z); 
-
-
-
+            rb.linearVelocity = new Vector3(moveDirection * speed, rb.linearVelocity.y, rb.linearVelocity.z);
         }
         else
         {
@@ -56,31 +54,31 @@ void Start()
     }
     void climbCurrentLadder()
     {
+
         // move towards ladders[currentladder]
-        if (ladders[currentLadder].top.position.x - transform.position.x < 0)
+        if (Mathf.Abs(ladders[currentLadder].top.position.x - transform.position.x) > 0.1f)
         {
             // rb.AddForce(Vector3.left * speed * Time.deltaTime); //commented out because the movement on the ground wasn't really moving in the left/right direction
             float direction = Mathf.Sign(ladders[currentLadder].bottom.position.x - transform.position.x);
             rb.linearVelocity = new Vector3(direction * speed, rb.linearVelocity.y, rb.linearVelocity.z);
-            
+
         }
         else
         {
             // rb.AddForce(Vector3.right * speed * Time.deltaTime);
+            rb.linearVelocity = new Vector3(0, rb.linearVelocity.y, rb.linearVelocity.z);
             climbUpLadder(ladders[currentLadder]);
             currentLadder = (currentLadder + 1) % ladders.Length;
         }
     }
 
-    void OnTriggerEnter(Collider collision){ //changed to OnTriggerEnter because ladder uses isTrigger in its collider
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            Debug.Log("Player hit by fireball!");
-        }
+    void OnTriggerEnter(Collider collision)
+    { //changed to OnTriggerEnter because ladder uses isTrigger in its collider
+
         if (collision.gameObject.CompareTag("Ladder"))
         {
             Debug.Log("Fireball appraoched ladder");
-            if(ladders[currentLadder].top.position.y !=collision.gameObject.transform.position.y)
+            if (ladders[currentLadder].top.position.y != collision.gameObject.transform.position.y)
             {
                 Debug.Log("Can climb up this ladder");
                 climbUpLadder(ladders[currentLadder]);
@@ -92,11 +90,22 @@ void Start()
         }
 
     }
-    
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Mario"))
+        {
+            Debug.Log("Player hit by fireball!");
+
+            GameManager.Instance.RemoveLife();
+            Destroy(gameObject);
+        }
+    }
+
     void climbUpLadder(Ladder ladder)
     {
         Debug.Log("Climbing up ladder");
+        rb.isKinematic = true;
         this.transform.position = ladder.top.position;
-
+        rb.isKinematic = false;
     }
 }
